ac53466f0c265f61e22153bf838aeac2cbbbf6c7
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2022-12-02 08:12:38 +0100

    Corrected stupid spelling mistake

--- d2/a.mjs
+++ d2/a.mjs
@@ -5,35 +5,35 @@ const lines = text.split('\n');
 
 const rockScore = 1;
 const paperScore = 2;
-const siccorScore = 3;
+const scissorScore = 3;
 const winScore = 6;
 const drawScore = 3;
 
 const total = lines.reduce((sum, line) => {
   // A Rock
   // B Paper
-  // C Siccor
+  // C Scissor
   // X Rock
   // Y Paper
-  // Z Siccor
+  // Z Scissor
   if (line[0] === 'A' && line[2] === 'X') {
     return sum + rockScore + drawScore;
   } else if (line[0] === 'A' && line[2] === 'Y') {
     return sum + paperScore + winScore;
   } else if (line[0] === 'A' && line[2] === 'Z') {
-    return sum + siccorScore;
+    return sum + scissorScore;
   } else if (line[0] === 'B' && line[2] === 'X') {
     return sum + rockScore;
   } else if (line[0] === 'B' && line[2] === 'Y') {
     return sum + paperScore + drawScore;
   } else if (line[0] === 'B' && line[2] === 'Z') {
-    return sum + siccorScore + winScore;
+    return sum + scissorScore + winScore;
   } else if (line[0] === 'C' && line[2] === 'X') {
     return sum + rockScore + winScore;
   } else if (line[0] === 'C' && line[2] === 'Y') {
     return sum + paperScore;
   } else if (line[0] === 'C' && line[2] === 'Z') {
-    return sum + siccorScore + drawScore;
+    return sum + scissorScore + drawScore;
   }
 }, 0);
 
--- d2/b.mjs
+++ d2/b.mjs
@@ -5,19 +5,19 @@ const lines = text.split('\n');
 
 const rockScore = 1;
 const paperScore = 2;
-const siccorScore = 3;
+const scissorScore = 3;
 const winScore = 6;
 const drawScore = 3;
 
 const total = lines.reduce((sum, line) => {
   // A Rock
   // B Paper
-  // C Siccor
+  // C Scissor
   // X Lose
   // Y Draw
   // Z Win
   if (line[0] === 'A' && line[2] === 'X') {
-    return sum + siccorScore;
+    return sum + scissorScore;
   } else if (line[0] === 'A' && line[2] === 'Y') {
     return sum + rockScore + drawScore;
   } else if (line[0] === 'A' && line[2] === 'Z') {
@@ -27,11 +27,11 @@ const total = lines.reduce((sum, line) => {
   } else if (line[0] === 'B' && line[2] === 'Y') {
     return sum + paperScore + drawScore;
   } else if (line[0] === 'B' && line[2] === 'Z') {
-    return sum + siccorScore + winScore;
+    return sum + scissorScore + winScore;
   } else if (line[0] === 'C' && line[2] === 'X') {
     return sum + paperScore;
   } else if (line[0] === 'C' && line[2] === 'Y') {
-    return sum + siccorScore + drawScore;
+    return sum + scissorScore + drawScore;
   } else if (line[0] === 'C' && line[2] === 'Z') {
     return sum + rockScore + winScore;
   }
