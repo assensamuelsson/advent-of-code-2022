d3fbbcac623fac6fb1d7d1a6799484dfbde49152
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2022-12-14 05:55:53 +0100

    Added A star to day 12

--- d12/solution.mjs
+++ d12/solution.mjs
@@ -26,15 +26,13 @@ export const parseGraph = (text, startValue = 'a', endValue = 'z') => {
     for (let c = 0; c < nCols; c++) {
       let currentId = getNodeName(textMatrix, r, c);
 
-      if (currentId === 'end') {
-        graph[currentId] = null;
-      } else {
-        graph[currentId] = {};
-  
-        if (r >= 1 && heightMap[r - 1][c] - heightMap[r][c] <= 1) graph[currentId][getNodeName(textMatrix, r - 1, c)] = 1;
-        if (c <= nCols - 2 && heightMap[r][c + 1] - heightMap[r][c] <= 1) graph[currentId][getNodeName(textMatrix, r, c + 1)] = 1;
-        if (r <= nRows - 2 && heightMap[r + 1][c] - heightMap[r][c] <= 1) graph[currentId][getNodeName(textMatrix, r + 1, c)] = 1;
-        if (c >= 1 && heightMap[r][c - 1] - heightMap[r][c] <= 1) graph[currentId][getNodeName(textMatrix, r, c - 1)] = 1;
+      graph[currentId] = { pos: { x: c, y: r } };
+      if (currentId !== 'end') {
+        graph[currentId].children = {};
+        if (r >= 1 && heightMap[r - 1][c] - heightMap[r][c] <= 1) graph[currentId].children[getNodeName(textMatrix, r - 1, c)] = 1;
+        if (c <= nCols - 2 && heightMap[r][c + 1] - heightMap[r][c] <= 1) graph[currentId].children[getNodeName(textMatrix, r, c + 1)] = 1;
+        if (r <= nRows - 2 && heightMap[r + 1][c] - heightMap[r][c] <= 1) graph[currentId].children[getNodeName(textMatrix, r + 1, c)] = 1;
+        if (c >= 1 && heightMap[r][c - 1] - heightMap[r][c] <= 1) graph[currentId].children[getNodeName(textMatrix, r, c - 1)] = 1;
       }
     }
   }
@@ -66,7 +64,7 @@ const djikstras = (graph) => {
     unprocessed.delete(currentNodeName);
     const currentNode = graph[currentNodeName];
 
-    for (const [childNodeName, weight] of Object.entries(currentNode)) {
+    for (const [childNodeName, weight] of Object.entries(currentNode.children)) {
       if (unprocessed.has(childNodeName)) {
         const alt = dist[currentNodeName] + weight;
         if (alt < dist[childNodeName]) {
@@ -86,6 +84,55 @@ const djikstras = (graph) => {
 
   return path.reverse();
 };
+
+const astar = (graph, h) => {
+  const reconstructPath = (cameFrom, current) => {
+    const totalPath = [current];
+    while (Object.keys(cameFrom).includes(current)) {
+      current = cameFrom[current];
+      totalPath.push(current);
+    }
+    return totalPath.reverse();
+  }
+
+  const openSet = new Set(['start']);
+
+  const cameFrom = {};
+  const gScore = {};
+  const fScore = {};
+
+  Object.keys(graph).forEach(nodeName => {
+    gScore[nodeName] = Infinity;
+    fScore[nodeName] = Infinity;
+  });
+  gScore.start = 0;
+  fScore.start = h(graph.start.pos, graph.end.pos);
+
+  while(openSet.size) {
+    const currentNodeName = [...openSet].reduce((min, nodeName) => {
+      return fScore[nodeName] < min.score ? {score: fScore[nodeName], nodeName} : min;
+    }, {score: Infinity, nodeName: null}).nodeName;
+
+    if (currentNodeName === 'end') {
+      return reconstructPath(cameFrom, currentNodeName);
+    }
+
+    openSet.delete(currentNodeName);
+    for (const [childName, weight] of Object.entries(graph[currentNodeName].children)) {
+      const tentativeGScore = gScore[currentNodeName] + weight;
+      if (tentativeGScore < gScore[childName]) {
+        cameFrom[childName] = currentNodeName;
+        gScore[childName] = tentativeGScore;
+        fScore[childName] = tentativeGScore + h(graph[childName].pos, graph.end.pos);
+        if (!openSet.has(childName)) {
+          openSet.add(childName);
+        }
+      }
+    }
+  }
+}
+
+const manhattanDistance = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 
 const getNodeNamesWithSpecificHeight = (text, height = 'a') => {
   const textMatrix = text.split('\n').map(row => row.split(''));
@@ -112,7 +159,8 @@ const part1 = (text) => {
   console.log('Part 1:', path.length - 1);
 }
 
-const part2 = (text) => {
+const part2 = (text, algorithm) => {
+  const start = new Date();
   const startingNodeNames = getNodeNamesWithSpecificHeight(text, 'a');
   let minSteps = Infinity;
   console.log('Solving', startingNodeNames.length, 'graphs');
@@ -123,15 +171,16 @@ const part2 = (text) => {
     graph.start = graph[nodeName];
     delete graph[nodeName];
 
-    const path = djikstras(graph);
+    const path = algorithm(graph);
     minSteps = (path && path.length < minSteps) ? path.length : minSteps;
   })
 
-  console.log('Part 2:', minSteps - 1);
+  console.log('Part 2:', minSteps - 1, 'It took', new Date().getTime() - start.getTime(), 'ms');
 }
 
 if (process.argv[2]) {
   const text = fs.readFileSync(process.argv[2], 'utf-8');
   part1(text);
-  part2(text);
+  //part2(text, djikstras);
+  part2(text, (graph) => astar(graph, manhattanDistance));
 }
